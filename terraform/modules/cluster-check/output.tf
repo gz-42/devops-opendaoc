c1bc49076e72e37168ec0f486c0bf9b5090e27be
gz-42 <146101379+gz-42@users.noreply.github.com>
2025-03-05 23:31:11 +0100

    Delete terraform directory
    Still need work on it

--- terraform/modules/cluster-check/output.tf
+++
@@ -1,4 +0,0 @@
-output "cluster_ready" {
-  description = "Indicator that the cluster is ready"
-  value       = null_resource.ensure_cluster_ready.id
-}
--- terraform/output.tf
+++
@@ -1,4 +0,0 @@
-output "argocd_grafana_load_balancer_hostname" {
-  description = "The hostname of the load balancer"
-  value       = module.ingress.load_balancer_hostname
-}
