5eced28fcd4ea6010495c184e3ff81b1f4348e9d
gz-42 <146101379+gz-42@users.noreply.github.com>
2025-03-13 01:24:03 +0100

    Update main.tf
    We going on prod !

--- terraform/modules/cert-manager/main.tf
+++ terraform/modules/cert-manager/main.tf
@@ -6,8 +6,8 @@ module "cert_manager" {
   cluster_issuer_email                    = var.email
   cluster_issuer_name                     = "letsencrypt-${var.profile}"
   cluster_issuer_private_key_secret_name  = "letsencrypt-${var.profile}"
-  cluster_issuer_server                   = "https://acme-staging-v02.api.letsencrypt.org/directory"
-  #cluster_issuer_server                   = "https://acme-v02.api.letsencrypt.org/directory"
+  #cluster_issuer_server                   = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  cluster_issuer_server                   = "https://acme-v02.api.letsencrypt.org/directory"
   
   solvers = [
     {
